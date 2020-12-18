0d2e13a379ce5d5ec55f7b16cc899c240001ffe2
mjOmandam07 <mjomandam@gmail.com>
2020-12-18 23:02:31 +0800

    added the rate UI, rate form, and backend

--- cuisina_app/forms.py
+++ cuisina_app/forms.py
@@ -1,6 +1,6 @@
 from flask_wtf import FlaskForm
 from flask_wtf.file import FileField, FileAllowed
-from wtforms import StringField, SubmitField, SelectField, TextAreaField, SelectField
+from wtforms import StringField, SubmitField, SelectField, TextAreaField, SelectField, RadioField
 from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
 import cuisina_app.models as models
 
@@ -13,6 +13,10 @@ class CreatePost(FlaskForm):
 	upload_picture = FileField('Upload Picture',  validators=[FileAllowed(['jpg', 'png'])])
 	cuisine = SelectField('Cusine', choices=[('','Select Cuisine'),('Filipino','Filipino'),('Western','Western'),('Asian','Asian'),('European','European')], validators=[DataRequired()])
 	submit = SubmitField('Post')
+
+class rateForm(FlaskForm):
+    rate = RadioField('rate', choices=[('1','★'),('2','★'),('3','★'),('4','★'),('5','★')])
+    submit = SubmitField('Rate it')
 
 class addCommment(FlaskForm):
 	comment = TextAreaField('Content', validators=[DataRequired()])
--- cuisina_app/routes.py
+++ cuisina_app/routes.py
@@ -3,7 +3,7 @@ import secrets
 from PIL import Image
 from flask import render_template, redirect, request, url_for, flash, jsonify
 from cuisina_app import app
-from cuisina_app.forms import CreatePost, addCommment
+from cuisina_app.forms import CreatePost, addCommment, rateForm
 
 import cuisina_app.models as models
 
@@ -19,8 +19,8 @@ recipe = [
 		'saved':False,
 		'cuisine':'Western',
 		'timeDate':'December 2020',
-		'comment':'sample comment',
-		'rate':5,
+		'rate': 5,
+
 		'image': '/static/images/default.jpg',
 	},
 	{
@@ -32,8 +32,7 @@ recipe = [
 		'saved':False,
 		'cuisine':'Asian',
 		'timeDate':'December 2020',
-		'comment':'sample comment',
-		'rate':5,
+		'rate': 4,
 		'image':None,
 	},
 	{
@@ -45,8 +44,9 @@ recipe = [
 		'saved':False,
 		'cuisine':'European',
 		'timeDate':'December 2020',
-		'comment':'sample comment',
-		'rate':5,
+		'rate': 3,
+
+
 		'image':None,
 	},
 	{
@@ -58,8 +58,7 @@ recipe = [
 		'saved':True,
 		'cuisine':'Filipino',
 		'timeDate':'December 2020',
-		'comment':'sample comment',
-		'rate':5,
+		'rate': 2,
 		'image':None,
 	}
 ]
@@ -82,7 +81,35 @@ comments=[
 	}
 ]
 
-saved = []
+rating = [
+		{
+			'rate_id': 1,
+			'recipe_id': 1,
+			'rate': 5
+		},
+
+		{
+			'rate_id': 2,
+			'recipe_id': 2,
+			'rate': 4
+		},
+
+		{
+			'rate_id': 3,
+			'recipe_id': 3,
+			'rate': 3
+		},
+
+		{
+			'rate_id': 4,
+			'recipe_id': 4,
+			'rate': 2
+		}
+		
+]
+
+
+
 
 @app.route('/')
 def filter():
@@ -111,9 +138,9 @@ def home(fltr):
 		'status':'public',
 		'cuisine':'Filipino',
 		'timeDate':'December 2020',
-		'rate':5,
 		'image':None
 	}
+
 
 	
 	if form.validate_on_submit() and request.method == 'POST':
@@ -126,7 +153,7 @@ def home(fltr):
 		recipe.append(newPost)
 		return redirect(url_for('viewpost', recipe_id=newPost['recipe_id']))
 
-	return render_template('home.html', active='home', user=user, suggested_chef=user, recipe=recipe_to_view, form=form)
+	return render_template('home.html', active='home', user=user, suggested_chef=user, recipe=recipe_to_view, form=form, rating=rating)
 
 
 
@@ -144,21 +171,40 @@ def viewpost(recipe_id):
 		if i['recipe_id'] == recipe_id:
 			lastpost = i
 			user = lastpost['username']
+
+	for i in rating:
+		if i['recipe_id'] == int(recipe_id):
+			rate = i['rate']
 	newComment = {
 		'user': user,
 		'recipe_id':lastpost['recipe_id'],
 		'content':'',
 		'timeDate':'December 2020'
 	}
+
 	form = addCommment()
+	form_rate = rateForm()
+
 	
-	if form.validate_on_submit() and request.method == 'POST':
+	
+	if form.validate_on_submit():
 		newComment['content'] = form.comment.data
 		comments.append(newComment)
 		return redirect(url_for('viewpost', recipe_id=lastpost['recipe_id']))
+	elif form_rate.validate_on_submit():
+		return redirect(url_for('add_rate', recipe_id=lastpost['recipe_id'], rate=form_rate.rate.data))
+	return render_template('view-post.html',  active='home', user=user, suggested_chef=user, recipe=lastpost, comments=comment_to_view, form=form, form_rate=form_rate, rating = int(rate))
 
-	return render_template('view-post.html',  active='home', user=user, suggested_chef=user, recipe=lastpost, comments=comment_to_view, form=form)
 
+
+@app.route('/add_rate/<recipe_id>/<rate>')
+def add_rate(recipe_id, rate):
+	test = {}
+	for item in rating:
+		if item['recipe_id'] == int(recipe_id):
+			item['rate'] = int(rate)
+	return redirect(url_for('viewpost', recipe_id=recipe_id))
+	
 
 
 def save_picture(form_picture):
