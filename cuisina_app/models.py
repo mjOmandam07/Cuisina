5095743231bcaa1f4af1c11e8d3ce1c8bb2bb598
mjOmandam07 <mjomandam@gmail.com>
2020-12-26 09:47:12 +0800

    fixed the Post Recipe with pictures and used mysql database to save recipe, add rate and average rate

--- cuisina_app/models.py
+++ cuisina_app/models.py
@@ -1,49 +1,281 @@
 from cuisina_app import mysql
+from datetime import datetime
 
 
-''' 
 
-	HERE WE CAN PUT OUR SQL QUERIES WE CAN ADD OR REMOVE A QUERY
-
-	GIVEN THAT YOU HAVE ENOUGH KNOWLEDGE OF THE OTHER FEATURES
-
-
-	YOU CAN ADD OR REMOVE QUERY ONLY BASING ON THE  FEATURE YOU ARE DOING
-
-
-	THE CURRENT VARIABLES/ATTRIBUTES ARE THE VARIABLE/ATTRIBUTE FOR
-
-
-	THE USER TABLE, YOU CAN CHANGE, ADD, REMOVE (ONLY) THE VARIABLEs/ATTRIBUTES
-
-
-	ACCORDING TO THE TABLE YOU ARE MANIPULATING CONSIDERING
-
-
-	THE FEATURE YOU ARE WORKING
-
-
-	*WE SHOULD NOT PUT ANY QUERRIES ON THE ROUTE.PY*
-
-
-'''
 class chef(object):
-	def __init__(self, user_id=None, username=None,
-				 password=None, email_address=None):
+	def __init__(self, filter=None, user_id=None, username=None,
+				 password=None, email_address=None,
+
+				 recipe_id=None, title=None, description=None, status=None, time_date=None, cuisine=None, saved=None,
+
+				 comment_id=None, content=None,
+
+				 rate_id=None, rating = None, isRated = None,
+
+				 image_id=None, filename=None):
+
+		self.filter = filter
 
 		self.user_id = user_id
 		self.username = username
 		self.password = password
 		self.email_address = email_address
 
+		self.recipe_id = recipe_id
+		self.title = title
+		self.description = description
+		self.status = status
+		self.time_date = datetime.now()
+		self.cuisine = cuisine
+		self.saved = saved
+
+		self.comment_id = comment_id
+		self.content = content
+
+		self.rate_id = rate_id
+		self.rating = rating
+		self.isRated = isRated
+
+		self.image_id = image_id
+		self.filename = filename
+
 
 
 ##############SAMPLE QUERY######################
-	def allUser(self):
+	def sampleCurrentUser(self):
 		cursor = mysql.connection.cursor()
-		sql = "SELECT * FROM user"
+		sql = "SELECT * FROM user WHERE user_id = 1"
 
 		cursor.execute(sql)
 		display = cursor.fetchall()
 		return display
 ################################################
+
+
+
+
+
+
+	def viewRecipes(self):
+		cursor = mysql.connection.cursor()
+		sql = """"""
+
+		if self.filter == 'All':
+			sql = """SELECT r.*,i.filename, u.username FROM
+					 ((images AS i RIGHT JOIN recipe as r ON i.recipe_id = r.recipe_id )
+					 	 JOIN user AS u ON u.user_id = r.user_id)
+					 	 	 ORDER BY r.time_date"""
+
+		elif self.filter == 'Western':
+			sql = """SELECT r.*,i.filename, u.username FROM
+					 ((images AS i RIGHT JOIN recipe as r ON i.recipe_id = r.recipe_id )
+					 	 JOIN user AS u ON u.user_id = r.user_id)
+					 	 	WHERE cuisine = 'Western' ORDER BY r.time_date"""
+
+		elif self.filter == 'Asian':
+			sql = """SELECT r.*,i.filename, u.username FROM
+					 ((images AS i RIGHT JOIN recipe as r ON i.recipe_id = r.recipe_id )
+					 	 JOIN user AS u ON u.user_id = r.user_id)
+					 	 	WHERE cuisine = 'Asian' ORDER BY r.time_date"""
+
+		elif self.filter == 'European':
+			sql = """SELECT r.*,i.filename, u.username FROM
+					 ((images AS i RIGHT JOIN recipe as r ON i.recipe_id = r.recipe_id )
+					 	 JOIN user AS u ON u.user_id = r.user_id)
+					 	 	WHERE cuisine = 'European' ORDER BY r.time_date"""
+
+		elif self.filter == 'Filipino':
+			sql = """SELECT r.*,i.filename, u.username FROM
+					 ((images AS i RIGHT JOIN recipe as r ON i.recipe_id = r.recipe_id )
+					 	 JOIN user AS u ON u.user_id = r.user_id)
+					 	 	WHERE cuisine = 'Filipino' ORDER BY r.time_date"""
+
+
+
+		cursor.execute(sql)
+		display = cursor.fetchall()
+		return display
+
+
+	def viewSelectRecipe(self):
+		cursor = mysql.connection.cursor()
+		sql = """SELECT r.*,i.filename, u.username FROM ((images AS i RIGHT JOIN recipe as r ON i.recipe_id = r.recipe_id ) JOIN user AS u ON u.user_id = r.user_id) WHERE r.recipe_id = '{}'""".format(self.recipe_id)
+
+		cursor.execute(sql)
+		display = cursor.fetchall()
+		return display
+
+
+	def uploadRecipePicture(self):
+		cursor = mysql.connection.cursor()
+		sql = """SELECT recipe_id FROM recipe WHERE recipe_id = (SELECT max(recipe_id) FROM recipe)"""
+
+		cursor.execute(sql)
+		display = cursor.fetchall()
+
+
+		sql2 = """INSERT INTO images (filename, recipe_id) VALUES ('%s', %d)""" % (self.filename, display[0][0])
+
+		cursor.execute(sql2)
+		mysql.connection.commit()
+
+
+
+
+	def addRecipes(self):
+		cursor = mysql.connection.cursor()
+		sql = """INSERT INTO recipe (title, description, time_date, cuisine, user_id)
+					 VALUES ('%s', '%s', '%s','%s',%d)""" % (self.title,
+					 													self.description, 
+					 													 self.time_date,
+					 													 self.cuisine, 
+					 													 self.user_id)
+	
+		cursor.execute(sql)
+		mysql.connection.commit()
+
+		sql2 = """SELECT recipe_id FROM recipe WHERE recipe_id = (SELECT max(recipe_id) FROM recipe)"""
+
+		cursor.execute(sql2)
+		display = cursor.fetchall()
+
+		return display[0][0]
+
+
+
+	def saveRecipe(self):
+		cursor = mysql.connection.cursor()
+
+		if self.saved == 0:
+			print(self.saved, "True before")
+			sql = """ UPDATE recipe SET saved = TRUE WHERE recipe_id = '{}' """.format(self.recipe_id)
+		else:
+			sql = """ UPDATE recipe SET saved = FALSE WHERE recipe_id = '{}' """.format(self.recipe_id)
+
+		cursor.execute(sql)
+		mysql.connection.commit()
+
+
+
+	def viewSavedRecipes(self):
+		cursor = mysql.connection.cursor()
+		sql = """"""
+
+		if self.filter == 'All':
+			sql = """SELECT r.*,i.filename, u.username FROM
+					 ((images AS i RIGHT JOIN recipe as r ON i.recipe_id = r.recipe_id )
+					 	 JOIN user AS u ON u.user_id = r.user_id)
+					 	 	 WHERE saved = TRUE ORDER BY r.time_date"""
+
+		elif self.filter == 'Western':
+			sql = """SELECT r.*,i.filename, u.username FROM
+					 ((images AS i RIGHT JOIN recipe as r ON i.recipe_id = r.recipe_id )
+					 	 JOIN user AS u ON u.user_id = r.user_id)
+					 	 	WHERE cuisine = 'Western' AND saved = TRUE ORDER BY r.time_date"""
+
+		elif self.filter == 'Asian':
+			sql = """SELECT r.*,i.filename, u.username FROM
+					 ((images AS i RIGHT JOIN recipe as r ON i.recipe_id = r.recipe_id )
+					 	 JOIN user AS u ON u.user_id = r.user_id)
+					 	 	WHERE cuisine = 'Asian' AND saved = TRUE ORDER BY r.time_date"""
+
+		elif self.filter == 'European':
+			sql = """SELECT r.*,i.filename, u.username FROM
+					 ((images AS i RIGHT JOIN recipe as r ON i.recipe_id = r.recipe_id )
+					 	 JOIN user AS u ON u.user_id = r.user_id)
+					 	 	WHERE cuisine = 'European' AND saved = TRUE ORDER BY r.time_date"""
+
+		elif self.filter == 'Filipino':
+			sql = """SELECT r.*,i.filename, u.username FROM
+					 ((images AS i RIGHT JOIN recipe as r ON i.recipe_id = r.recipe_id )
+					 	 JOIN user AS u ON u.user_id = r.user_id)
+					 	 	WHERE cuisine = 'Filipino' AND saved = TRUE ORDER BY r.time_date"""
+
+
+
+		cursor.execute(sql)
+		display = cursor.fetchall()
+		return display
+
+	def viewComments(self):
+		cursor = mysql.connection.cursor()
+		sql = """ SELECT c.*, u.username FROM can_comment as c JOIN user as u ON c.user_id = u.user_id WHERE recipe_id = '{}' """.format(self.recipe_id)
+
+		cursor.execute(sql)
+		display = cursor.fetchall()
+		return display
+
+
+
+	def addComment(self):
+		cursor = mysql.connection.cursor()
+
+		sql = """ INSERT INTO can_comment(content, time_date, user_id, recipe_id)
+					VALUES('%s', '%s', %d, %d) """ % (self.content, self.time_date, self.user_id, self.recipe_id)
+
+
+		cursor.execute(sql)
+		mysql.connection.commit()
+
+
+
+	def addRate(self):
+		cursor = mysql.connection.cursor()
+
+		sql=""""""
+
+		if self.isRated:
+			sql = """UPDATE can_rate SET rating = '{}'
+					WHERE  user_id = '{}' AND recipe_id = '{}'""".format(self.rating, self.user_id, self.recipe_id)
+		else:
+			sql = """INSERT INTO can_rate(rating, user_id, recipe_id)
+					VALUES('%s', %d, %d)""" % (self.rating, self.user_id, self.recipe_id)
+
+		cursor.execute(sql)
+		mysql.connection.commit()
+
+
+	def yourCurrentRate(self):
+		cursor = mysql.connection.cursor()
+
+		sql = """SELECT rating FROM can_rate WHERE rate_id = '{}' AND user_id = '{}'""".format(self.recipe_id,self.user_id)
+
+		cursor.execute(sql)
+		display = cursor.fetchall()
+		return display
+
+
+
+	def getAvgRate(self):
+		rates = []
+		rates.clear()
+		cursor = mysql.connection.cursor()
+		sql = """SELECT r.*,i.filename, u.username FROM
+					 ((images AS i RIGHT JOIN recipe as r ON i.recipe_id = r.recipe_id )
+					 	 JOIN user AS u ON u.user_id = r.user_id)
+					 	 	 ORDER BY r.time_date"""
+
+
+		cursor.execute(sql)
+		recipes = cursor.fetchall()
+
+		for item in recipes:
+			sql2 = "SELECT rating FROM can_rate WHERE recipe_id = {}".format(item[0])
+
+			cursor.execute(sql2)
+			display = cursor.fetchall()
+
+			for rate in display:
+				rates.append(rate[0])
+				get_avg = sum(rates) / len(rates)
+				average_rate = round(get_avg)
+				
+				sql3 = """UPDATE recipe SET avg_rating = '{}'
+					WHERE recipe_id = '{}'""".format(average_rate, item[0])
+
+				cursor.execute(sql3)
+				mysql.connection.commit()
+
+
+		
--- cuisina_app/routes.py
+++ cuisina_app/routes.py
@@ -9,78 +9,6 @@ import cuisina_app.models as models
 
 
 
-recipe = [
-	{
-		'recipe_id': 1,
-		'username':'sampleUser',
-		'title':'My first Western Recipe',
-		'description': 'At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi',
-		'status':'public',
-		'saved':False,
-		'cuisine':'Western',
-		'timeDate':'December 2020',
-		'rate': 5,
-
-		'image': '/static/images/default.jpg',
-	},
-	{
-		'recipe_id': 2,
-		'username':'sampleUser2',
-		'title':'My Second Asian Recipe',
-		'description': 'At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi',
-		'status':'public',
-		'saved':False,
-		'cuisine':'Asian',
-		'timeDate':'December 2020',
-		'rate': 4,
-		'image':None,
-	},
-	{
-		'recipe_id': 3,
-		'username':'sampleUser2',
-		'title':'My Second European Recipe',
-		'description': 'At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi',
-		'status':'public',
-		'saved':False,
-		'cuisine':'European',
-		'timeDate':'December 2020',
-		'rate': 3,
-
-
-		'image':None,
-	},
-	{
-		'recipe_id': 4,
-		'username':'sampleUser2',
-		'title':'My Second  Filipino Recipe',
-		'description': 'At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi',
-		'status':'public',
-		'saved':False,
-		'cuisine':'Filipino',
-		'timeDate':'December 2020',
-		'rate': 2,
-		'image':None,
-	}
-]
-
-
-
-comments=[
-	{
-		'user':'commentor1',
-		'recipe_id':1,
-		'content': 'At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi',
-		'timeDate':'December 2020'
-	},
-
-	{
-		'user':'commentor2',
-		'recipe_id':2,
-		'content': 'At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi',
-		'timeDate':'December 2020'
-	}
-]
-
 rating = [
 		{
 			'rate_id': 1,
@@ -118,96 +46,51 @@ def filter():
 
 @app.route('/home/<string:fltr>', methods=['GET', 'POST'])
 def home(fltr):
-	recipe_to_view = []
-
-	for i in recipe:
-		user = i['username']
-		if fltr != 'All':
-			if i['cuisine'] == fltr:
-				recipe_to_view.append(i)
-		else:
-			recipe_to_view = recipe
-	lastpost = recipe[-1]
-	last_id = lastpost['recipe_id']
+	db = models.chef(filter=fltr)
+	db.getAvgRate()
+	recipe = db.viewRecipes()
 	form = CreatePost()
-	newPost = {
-		'recipe_id': last_id + 1,
-		'username':user,
-		'title':'',
-		'description': '',
-		'status':'public',
-		'cuisine':'Filipino',
-		'timeDate':'December 2020',
-		'rate': 0,
-		'image':None,
-		'saved':False
-
-	}
-
-	lastrate = rating[-1]
-	lastrate_id = lastrate['rate_id']
-	lastrate_recipe = lastrate['recipe_id']
-	newRate = {
-		'rate_id':lastrate_id + 1,
-		'recipe_id':lastrate_recipe + 1,
-		'rate': 0
-
-	}
-
-
-	
+	user = db.sampleCurrentUser()
 	if form.validate_on_submit() and request.method == 'POST':
-		newPost['title'] = form.title.data
-		newPost['description'] = form.content.data
-		newPost['cuisine'] = form.cuisine.data
+		create = models.chef(title = form.title.data,
+							 description = form.content.data,
+							  cuisine = form.cuisine.data,
+							  user_id=user[0][0])
+		latest = create.addRecipes()
 		if form.upload_picture.data:
 			picture_file = save_picture(form.upload_picture.data)
-			newPost['image'] = url_for('static', filename='user_images/' + picture_file)
-		recipe.append(newPost)
-		rating.append(newRate)
-		return redirect(url_for('viewpost', recipe_id=newPost['recipe_id']))
+			picture = url_for('static', filename='posted-recipe_images/' + picture_file)
+			upload_picture = models.chef(filename = picture)
+			upload_picture.uploadRecipePicture()
+		return redirect(url_for('viewpost', recipe_id= latest))
 
-	return render_template('home.html', active='home', user=user, suggested_chef=user, recipe=recipe_to_view, form=form, rating=rating)
+	return render_template('home.html', active='home', user=user, suggested_chef=user, recipe=recipe, form=form)
 
 
 
 @app.route('/viewpost/<int:recipe_id>', methods=['GET', 'POST'])
 def viewpost(recipe_id):
-	lastpost = {}
-	rate = int
-	user = str
-	comment_to_view = []
+	db = models.chef(recipe_id=recipe_id)
+	recipe = db.viewSelectRecipe()
+	user = db.sampleCurrentUser()
 
-	for item in comments:
-		if item['recipe_id'] == recipe_id:
-			comment_to_view.append(item)
-
-	for i in recipe:
-		if i['recipe_id'] == recipe_id:
-			lastpost = i
-			user = lastpost['username']
-
-	for i in rating:
-		if i['recipe_id'] == int(recipe_id):
-			rate = i['rate']
-
-	newComment = {
-		'user': user,
-		'recipe_id':lastpost['recipe_id'],
-		'content':'',
-		'timeDate':'December 2020'
-	}
+	comments = db.viewComments()
 
 	form = addCommment()
 	form_rate = rateForm()
+	currentRate = models.chef(recipe_id=recipe_id, user_id=user[0][0])
+	currentRating = currentRate.yourCurrentRate() 
 	
 	if form.validate_on_submit():
-		newComment['content'] = form.comment.data
-		comments.append(newComment)
-		return redirect(url_for('viewpost', recipe_id=lastpost['recipe_id']))
+		create_comment = models.chef(content = form.comment.data, user_id=user[0][0], recipe_id=recipe_id)
+
+		create_comment.addComment()
+		return redirect(url_for('viewpost', recipe_id=recipe_id))
 	elif form_rate.validate_on_submit():
-		return redirect(url_for('add_rate', recipe_id=lastpost['recipe_id'], rate=form_rate.rate.data))
-	return render_template('view-post.html',  active='home', user=user, suggested_chef=user, recipe=lastpost, comments=comment_to_view, form=form, form_rate=form_rate, rating = int(rate))
+		rate = models.chef(recipe_id=recipe_id, user_id=user[0][0], rating=form_rate.rate.data, isRated=currentRating)
+		rate.addRate()
+		return redirect(url_for('viewpost', recipe_id=recipe_id))
+	return render_template('view-post.html',  active='home', user=user, suggested_chef=user, recipe=recipe, comments=comments, form=form, form_rate=form_rate, rate = currentRating)
 
 
 
@@ -224,7 +107,7 @@ def save_picture(form_picture):
 	random_hex = secrets.token_hex(8)
 	_, f_ext = os.path.splitext(form_picture.filename)
 	picture_fn = random_hex + f_ext
-	picture_path = os.path.join(app.root_path, 'static/user_images', picture_fn)
+	picture_path = os.path.join(app.root_path, 'static/posted-recipe_images', picture_fn)
 
 
 	output_size = (900, 900)
@@ -235,28 +118,26 @@ def save_picture(form_picture):
 	return picture_fn
 
 
+@app.route('/saved_filter')
+def saved_filter():
+	return redirect(url_for('savedRecipe', fltr='All'))
 
-@app.route('/savedRecipe')
-def savedRecipe():
-	saved = []
-	for i in recipe:
-		user = i['username']
-		if i['saved'] == True:
-			saved.append(i)
-	return render_template('saved_recipe.html', user=user, recipe = saved)
+@app.route('/savedRecipe/<string:fltr>')
+def savedRecipe(fltr):
+	db = models.chef(filter=fltr)
+	recipe = db.viewSavedRecipes()
+	user = db.sampleCurrentUser()
+	return render_template('saved_recipe.html', recipe = recipe, user=user, active='saved')
 
 
 @app.route('/saved/<recipe_id>')
 def saveRecipe(recipe_id):
-	for item in recipe:
-		if item['recipe_id'] == int(recipe_id) and item['saved'] == False:
-			item['saved'] = True
-			print(item)
-		elif item['recipe_id'] == int(recipe_id) and item['saved'] == True:
-			item['saved'] = False
-			print(item)
+	db = models.chef(recipe_id=recipe_id)
+	recipe = db.viewSelectRecipe()
+	save = models.chef(saved = recipe[0][5], recipe_id=recipe_id)
+	save.saveRecipe()
 	return redirect(url_for('viewpost', recipe_id=recipe_id))
-	#return render_template('sample.html', recipe=recipe)
+
 
 
 
