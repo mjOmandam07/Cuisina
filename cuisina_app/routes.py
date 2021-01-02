499e98dd2d864d34503846a9c952c35dcd640188
mjOmandam07 <mjomandam@gmail.com>
2021-01-02 23:23:11 +0800

    removed datetime

--- cuisina_app/routes.py
+++ cuisina_app/routes.py
@@ -1,5 +1,5 @@
 from flask import render_template, redirect, request, url_for, flash, session
-from datetime import timedelta
+
 from cuisina_app import app
 from cuisina_app.forms import LoginForm
 import cuisina_app.models as models
